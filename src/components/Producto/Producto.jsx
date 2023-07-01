0bc150ee70b09b537ce42fc9ee789ed5290d8339
Tomás D. Vazquez <tomasvazquezdev@gmail.com>
2023-07-01 16:05:33 -0300

    create maps and product cards

--- src/components/Producto/Producto.jsx
+++ src/components/Producto/Producto.jsx
@@ -1,16 +1,20 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material"
+import { Button, Card, CardActions, CardContent, CardMedia, Divider, Typography } from "@mui/material"
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
 
 const Producto = ({id, nombre, precio, descripcion, categoria, rareza, img}) => {
   return (
     <Card sx={{maxWidth: 350, margin: 5+'px'}}>
       <CardMedia component="img" image={img} height={200} width={350}/>
+      <Divider/>
       <CardContent>
         <Typography variant="h5">{nombre}</Typography>
         <Typography variant="body2">{descripcion}</Typography>
       </CardContent>
+      <Divider light />
       <CardActions>
-        <Button variant="contained" color="success">Add to cart</Button>
-        <Button color="error">Remove</Button>
+        <Button variant="contained" color="success"><AddShoppingCartIcon/></Button>
+        <Button variant="contained" color="error"><RemoveShoppingCartIcon/></Button>
       </CardActions>
     </Card>
   )
