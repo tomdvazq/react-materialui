64eed2b21a7f8bdf61f4ac25964ecc5d8250a0f3
Tomás D. Vazquez <tomasvazquezdev@gmail.com>
2023-07-05 22:39:56 -0300

    create context provider

--- src/context/CarritoContext.js
+++ src/context/CarritoContext.js
@@ -1,17 +1,55 @@
 import { useState, createContext } from "react";
 
 export const CarritoContexto = createContext({
-    carrito: [],
-    total: 0,
-    cantidadTotal: 0
-})
+  carrito: [],
+  total: 0,
+  cantidadTotal: 0,
+});
 
 export const CarritoProvider = () => {
-    const [carrito, setCarrito] = useState([]);
-    const [total, setTotal] = useState(0);
-    const [cantidadTotal, setCantidadTotal] = useState(0);
-    
-    const agregarProducto = (item, cantidad) => {
-        const productoExistente = carrito.find(prod => prod.item.id === item.id)
+  const [carrito, setCarrito] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [cantidadTotal, setCantidadTotal] = useState(0);
+
+  const agregarProducto = (item, cantidad) => {
+    const productoExistente = carrito.find((prod) => prod.item.id === item.id);
+    if (!productoExistente) {
+      setCarrito((prev) => [...prev, { item, cantidad }]);
+      setCantidadTotal((prev) => prev + cantidad);
+      setTotal((prev) => prev + item.precio * cantidad);
+    } else {
+      const carritoActualizado = carrito.map((prod) => {
+        if (prod.item.id === item.id) {
+          return { ...prod, cantidad: prod.cantidad + cantidad };
+        } else {
+          return prod;
+        }
+      });
+      setCarrito(carritoActualizado);
+      setCantidadTotal((prev) => prev + cantidad);
+      setTotal((prev) => {
+        item.precio * cantidad;
+      });
     }
-}
+  };
+ 
+  const eliminarProducto = (id) => {
+    const productoEliminado = carrito.find(prod => prod.item.id === id);
+    const carritoActualizado = carrito.filter(prod => prod.item.id !== id);
+    setCarrito(carritoActualizado);
+    setCantidadTotal(prev => prev - productoEliminado.cantidad);
+    setTotal(prev => prev - (productoEliminado.item.precio * productoEliminado.cantidad));
+  }
+
+  const vaciarCarrito = () => {
+    setCarrito([]);
+    setCantidadTotal(0);
+    setTotal(0);
+  }
+
+  return (
+    <CarritoContexto.Provider value={{carrito, total, cantidadTotal, agregarProducto, eliminarProducto, vaciarCarrito}}>
+        
+    </CarritoContexto.Provider>
+  )
+};
