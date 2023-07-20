dd5e7c93cff3925ad4aed649c40a0cbee52c5eb3
Tomás D. Vazquez <tomasvazquezdev@gmail.com>
2023-07-19 22:58:03 -0300

    design cart

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,27 +2,27 @@ import { useContext } from "react";
 import { CarritoContext } from "../../context/CarritoContext";
 import { NavLink } from "react-router-dom";
 import { Button } from "@mui/material";
+import '../CartWidget/CartWidget.css';
 
 const CartWidget = ({navArrayLinks}) => {
     const {cantidadTotal} = useContext(CarritoContext);
 
   return (
-    <div>
+    <div id="cart">
       {navArrayLinks.map((item) =>
-        item.isCart ? ( // Utilizamos la propiedad isCart para identificar el elemento del carrito
+        item.isCart ? ( 
           <Button
             sx={{ color: "white" }}
             key={item.title}
+            className="cart"
             component={NavLink}
             to={item.path}
           >
             {item.title}
+            {cantidadTotal > 0 ? <strong>{cantidadTotal}</strong> : null}
           </Button>
         ) : null
       )}
-      {
-        cantidadTotal > 0 ? <strong>{cantidadTotal}</strong> : null
-      }
     </div>
   );
 };
